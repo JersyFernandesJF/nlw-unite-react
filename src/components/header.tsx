0e6d08599aec38eaef49e3c10f6ea8a84b04c2cf
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-04-07 14:05:49 +0100

    header changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,16 @@
+
+import { NavLink } from "./nav-link"
 import nwlUniteIcon from "../assets/nlw-unite-icon.svg"
+
 
 export function Header(){
   return(
-    <div className="flex items-center gap-5">
-      <img src={nwlUniteIcon}/>
+    <div className="flex items-center gap-5 py-2">
+      <img src={nwlUniteIcon} alt="NLW Unite"/>
 
-      <nav>
-        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="" className="font-medium text-sm">Participantes</a>
+      <nav className="flex items-center gap-5">
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </div>
   )
